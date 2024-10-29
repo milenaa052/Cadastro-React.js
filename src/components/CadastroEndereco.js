a003ce8deecfd286365c266ce4f0f2e1cf352fb5
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2024-10-29 20:51:03 -0300

    Deixando um código limpo no arquivo cadastroEndereco.js

--- src/components/CadastroEndereco.js
+++ src/components/CadastroEndereco.js
@@ -16,22 +16,19 @@ const CadastroEndereco = ({ onNext }) => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setError({ ...error, [name]: '' });
+    setError((prevError) => ({ ...prevError, [name]: '' }));
 
     if (name === "numero" && value < 0) {
-      setError({ ...error, numero: "O número não deve ser negativo." });
+      setError((prevError) => ({ ...prevError, numero: "O número não deve ser negativo." }));
       return;
     }
 
-    setFormData({
-      ...formData,
-      [name]: value
-    });
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
   const buscarCep = async () => {
     if (formData.cep.length !== 8) {
-      setError({ ...error, cep: "O CEP deve ter 8 dígitos." });
+      setError((prevError) => ({ ...prevError, cep: "O CEP deve ter 8 dígitos." }));
       return;
     }
     
@@ -45,25 +42,28 @@ const CadastroEndereco = ({ onNext }) => {
         return;
       }
 
-      setFormData({
-        ...formData,
+      setFormData((prevData) => ({
+        ...prevData,
         rua: data.logradouro || '',
         cidade: data.localidade || '',
         estado: data.uf || ''
-      });
+      }));
+
     } catch (error) {
-      setError({ ...error, cep: "Erro ao buscar o CEP." });
+       setError((prevError) => ({ ...prevError, cep: "Erro ao buscar o CEP." }));
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(formData.cep === '') {
-      setError({ ...error, cep: "Preencha o seu CEP por favor." });
+    if(!formData.cep) {
+      setError((prevError) => ({ ...prevError, cep: "Preencha o seu CEP por favor." }));
       return;
-    } else if(formData.numero === '') {
-      setError({ ...error, numero: "Preencha o número da sua casa/apartamento por favor." });
+    } 
+    
+    if(!formData.numero) {
+      setError((prevError) => ({ ...prevError, numero: "Preencha o número da sua casa/apartamento por favor." }));
       return;
     }
 
@@ -77,32 +77,36 @@ const CadastroEndereco = ({ onNext }) => {
 
         <form onSubmit={handleSubmit} className="form">
           <div className="campos">
-              <label htmlFor="cep" className="label">CEP</label>
-              <input type="text" id="cep" name="cep" value={formData.cep} onChange={handleChange} onBlur={buscarCep}/>
-              <a href="https://buscacepinter.correios.com.br/app/endereco/index.php" target="blank" class="buscaCep">Não sei meu cep</a>
-              {error.cep && <span className="error" style={{float: 'right'}}>{error.cep}</span>}
+            <label htmlFor="cep" className="label">CEP</label>
+            <input type="text" id="cep" name="cep" value={formData.cep} onChange={handleChange} onBlur={buscarCep}/>
+            <a href="https://buscacepinter.correios.com.br/app/endereco/index.php" target="blank" class="buscaCep">Não sei meu cep</a>
+            {error.cep && <span className="error" style={{float: 'right'}}>{error.cep}</span>}
           </div>
+
           <div className="campos">
-              <label htmlFor="estado" className="label">Estado</label>
-              <input type="text" id="estado" name="estado" value={formData.estado} onChange={handleChange} disabled/>
+            <label htmlFor="estado" className="label">Estado</label>
+            <input type="text" id="estado" name="estado" value={formData.estado} onChange={handleChange} disabled/>
           </div>
+
           <div className="campos">
-              <label htmlFor="cidade" className="label">Cidade</label>
-              <input type="text" id="cidade" name="cidade" value={formData.cidade} onChange={handleChange} disabled/>
+            <label htmlFor="cidade" className="label">Cidade</label>
+            <input type="text" id="cidade" name="cidade" value={formData.cidade} onChange={handleChange} disabled/>
           </div>
+
           <div className="campos">
-              <label htmlFor="rua" className="label">Rua</label>
-              <input type="text" id="rua" name="rua" value={formData.rua} onChange={handleChange} disabled/>
+            <label htmlFor="rua" className="label">Rua</label>
+            <input type="text" id="rua" name="rua" value={formData.rua} onChange={handleChange} disabled/>
           </div>
+
           <div className="campos">
-              <label htmlFor="numero" className="label">Número</label>
-              <input type="text" id="numero" name="numero" value={formData.numero} onChange={handleChange} required/>
-              {error.numero && <span className="error">{error.numero}</span>}
+            <label htmlFor="numero" className="label">Número</label>
+            <input type="text" id="numero" name="numero" value={formData.numero} onChange={handleChange} required/>
+            {error.numero && <span className="error">{error.numero}</span>}
           </div>
 
           <div className='submit'>
             <a href="/login" className="login">Fazer login</a>
-            <button type="button" onClick={handleSubmit}>Próximo</button>
+            <button type="submit" onClick={handleSubmit}>Próximo</button>
           </div>
         </form>
       </div>
